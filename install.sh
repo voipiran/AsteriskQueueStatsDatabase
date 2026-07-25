606b0da50056bb20a15ab930c049a3beb9f1c46b
Hamed Kouhfallah <54766086+voipiran@users.noreply.github.com>
2026-07-25 14:42:01 +0330

    remove bugs

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Install VOIPIRAN Stats"
-echo "VOIPIRAN.io"
+
+echo "======================================="
+echo " Install VOIPIRAN Queue Stats"
+echo " VOIPIRAN.io"
+echo "======================================="
 sleep 1
 
 echo "Install VOIPIRAN Asterisk Queue Database"
@@ -9,67 +12,131 @@ echo "Hamed Kouhfallah"
 echo "k.haamed@gmail.com"
 sleep 3
 
-###Fetch DB root PASSWORD
+##################################################
+# Get Issabel MySQL Root Password
+##################################################
+
 rootpw=$(sed -ne 's/.*mysqlrootpwd=//gp' /etc/issabel.conf)
 
-### Create Database
-echo "Install CallStats Mysql DataBase"
-echo "------------Create DB-----------------"
-mysql -uroot -p$rootpw < database/voipiran_stats-database.sql
-mysql -uroot -p$rootpw -e "GRANT ALL PRIVILEGES ON voipiran_stats.* TO 'root'@'localhost';"
+##################################################
+# Create Database
+##################################################
 
-#Asterisk Queue Adaptive
-sed -i '/\[options\]/a queue\_adaptive\_realtime\=no' /etc/asterisk/asterisk.conf
-sed -i '/\[options\]/a log\_membername\_as\_agent\=yes' /etc/asterisk/asterisk.conf
+echo "------------ Create Database ------------"
 
-### Add ODBC 
-echo "-------------odbc.ini----------------"
-echo "" >> /etc/odbc.ini
-echo "[voipiran_stats]" >> /etc/odbc.ini
-echo "driver=MariaDB" >> /etc/odbc.ini
-echo "server=localhost" >> /etc/odbc.ini
-echo "database=voipiran_stats" >> /etc/odbc.ini
-echo "Port=3306" >> /etc/odbc.ini
-echo "Socket=/var/lib/mysql/mysql.sock" >> /etc/odbc.ini
-echo "option=3" >> /etc/odbc.ini
-echo "charset=utf8" >> /etc/odbc.ini
+mysql -uroot -p"$rootpw" < database/voipiran_stats-database.sql
 
-### Add ODBC 
-echo "-------------res_odbc_custom.conf----------------"
-echo "" >> /etc/asterisk/res_odbc_custom.conf
-echo "[voipiran_stats]" >> /etc/asterisk/res_odbc_custom.conf
-echo "enabled=>yes" >> /etc/asterisk/res_odbc_custom.conf
-echo "dsn=>voipiran_stats" >> /etc/asterisk/res_odbc_custom.conf
-echo "pooling=>no" >> /etc/asterisk/res_odbc_custom.conf
-echo "limit=>1" >> /etc/asterisk/res_odbc_custom.conf
-echo "pre-connect=>yes" >> /etc/asterisk/res_odbc_custom.conf
-echo "username=>root" >> /etc/asterisk/res_odbc_custom.conf
-echo "password=>${rootpw}" >> /etc/asterisk/res_odbc_custom.conf
+mysql -uroot -p"$rootpw" -e \
+"GRANT ALL PRIVILEGES ON voipiran_stats.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;"
 
-	
-		 # اضافه کردن log_membername_as_agent در queues_custom.conf
-    QUEUE_CONF2="/etc/asterisk/queues_custom_general.conf"
-    SETTING="log_membername_as_agent = yes"
+##################################################
+# Asterisk Options
+##################################################
 
-    if [ ! -f "$QUEUE_CONF" ]; then
-        echo "⚠️ فایل $QUEUE_CONF وجود نداشت. در حال ساخت فایل..."
-        touch "$QUEUE_CONF"
-    fi
+ASTERISK_CONF="/etc/asterisk/asterisk.conf"
 
-    if grep -Fxq "$SETTING" "$QUEUE_CONF"; then
-        echo "✔️ $SETTING از قبل در $QUEUE_CONF وجود دارد"
-    else
-        echo "➕ افزودن $SETTING به $QUEUE_CONF"
-        echo "" >> "$QUEUE_CONF"
-        echo "; Added by script for voipiran stats" >> "$QUEUE_CONF"
-        echo "$SETTING" >> "$QUEUE_CONF"
-    fi
+grep -q "^queue_adaptive_realtime=no" "$ASTERISK_CONF" || \
+sed -i '/\[options\]/a queue_adaptive_realtime=no' "$ASTERISK_CONF"
 
+grep -q "^log_membername_as_agent=yes" "$ASTERISK_CONF" || \
+sed -i '/\[options\]/a log_membername_as_agent=yes' "$ASTERISK_CONF"
 
-### Add ODBC 
-echo "-------------extconfig.conf----------------"
-sed -i '/\[settings\]/a queue\_log \=\> odbc\,voipiran\_stats\,queue\_stats' /etc/asterisk/extconfig.conf
+##################################################
+# ODBC
+##################################################
 
+echo "------------ odbc.ini ------------"
 
-### Restart Asterisk
+if ! grep -q "^\[voipiran_stats\]" /etc/odbc.ini; then
+
+cat >> /etc/odbc.ini <<EOF
+
+[voipiran_stats]
+driver=MariaDB
+server=localhost
+database=voipiran_stats
+Port=3306
+Socket=/var/lib/mysql/mysql.sock
+option=3
+charset=utf8
+
+EOF
+
+fi
+
+##################################################
+# res_odbc_custom.conf
+##################################################
+
+echo "------------ res_odbc_custom.conf ------------"
+
+if ! grep -q "^\[voipiran_stats\]" /etc/asterisk/res_odbc_custom.conf; then
+
+cat >> /etc/asterisk/res_odbc_custom.conf <<EOF
+
+[voipiran_stats]
+enabled=>yes
+dsn=>voipiran_stats
+pooling=>no
+limit=>1
+pre-connect=>yes
+username=>root
+password=>${rootpw}
+
+EOF
+
+fi
+
+##################################################
+# queues_custom_general.conf
+##################################################
+
+QUEUE_CONF="/etc/asterisk/queues_custom_general.conf"
+
+SETTING="log_membername_as_agent = yes"
+
+echo "------------ queues_custom_general.conf ------------"
+
+touch "$QUEUE_CONF"
+
+if grep -Fxq "$SETTING" "$QUEUE_CONF"; then
+
+    echo "Already Exists"
+
+else
+
+cat >> "$QUEUE_CONF" <<EOF
+
+; Added by VOIPIRAN Queue Stats
+$SETTING
+
+EOF
+
+fi
+
+##################################################
+# extconfig.conf
+##################################################
+
+echo "------------ extconfig.conf ------------"
+
+if ! grep -q "^queue_log => odbc,voipiran_stats,queue_stats" /etc/asterisk/extconfig.conf; then
+
+sed -i '/\[settings\]/a queue_log => odbc,voipiran_stats,queue_stats' /etc/asterisk/extconfig.conf
+
+fi
+
+##################################################
+# Reload Asterisk
+##################################################
+
+echo "------------ Reload Asterisk ------------"
+
+asterisk -rx "module reload res_odbc.so"
 asterisk -rx "reload"
+
+echo
+echo "======================================="
+echo " VOIPIRAN Queue Stats Installed"
+echo "======================================="
