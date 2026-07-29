b5e40e1df4d48a6ae65afce2b8a3d1780a703f89
Hamed Kouhfallah <54766086+voipiran@users.noreply.github.com>
2026-07-29 12:56:22 +0330

    update

--- database/voipiran_stats-database.sql
+++ database/voipiran_stats-database.sql
@@ -4,8 +4,7 @@ SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-CREATE DATABASE `voipiran_stats` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `voipiran_stats` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `voipiran_stats`;
 
 CREATE TABLE `migrations` (
